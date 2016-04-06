06583ef3a4febff1456f58566e4f2e35db078307
Juan Ayala <juan.ayala.tech@gmail.com>
2016-04-06 12:51:54 -0400

    install maven via ark instead of yum because yum package was installing OpenJDK could not use the maven cookbook because it had a dependency on ark >= 1.0.0, and aem cookbook is lower.

--- site-cookbooks/aem-devstax/recipes/dev-packages.rb
+++ site-cookbooks/aem-devstax/recipes/dev-packages.rb
@@ -1,4 +1,26 @@
-%w(multitail apache-maven nano).each do |pkg|
+include_recipe 'ark::default'
+
+# install maven via ark
+version = '3.3.3'
+m2_home = '/usr/local/maven'
+ark 'maven' do
+    version version
+    url "http://apache.mirrors.tds.net/maven/maven-#{version.split('.')[0]}/#{version}/binaries/apache-maven-#{version}-bin.tar.gz"
+    home_dir m2_home
+    append_env_path true
+end
+
+# setup environmental variables
+template '/etc/mavenrc' do
+    source 'mavenrc.erb'
+    mode   '0755'
+    variables({
+        :m2_home => m2_home
+    })
+end
+
+# install other packages via yum
+%w(multitail nano).each do |pkg|
 
     yum_package pkg
 
